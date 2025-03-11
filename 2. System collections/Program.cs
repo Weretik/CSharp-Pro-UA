3fb6fc0dd85a696356c0e30d82371921637e92e7
Weretik <vitaliythupin@gmail.com>
2025-03-11 15:42:32 +0200

    Completed Task 2. Created collection to store customers and purchased product categories. Implemented functionality to get product categories by customer and find customers by category.

--- 2. System collections/Program.cs
+++ 2. System collections/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace _2._System_collections
+﻿using System.Text;
+
+namespace _2._System_collections
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            /*
+             * Завдання 2
+
+            Створіть колекцію, до якої можна додавати покупців та категорію придбаної ними продукції. 
+            З колекції можна отримувати категорії товарів, які купив покупець або за категорією визначити покупців.
+            */
+
+            var customerPurchases = new Dictionary<string, string>()
+            {
+                { "Віталій", "Телефони" },
+                { "Юра", "Блендер" },
+                { "Антон", "Телефони" }
+
+            };
+
+
+            Console.WriteLine("Віталій купив: " + customerPurchases["Віталій"]);
+            Console.WriteLine("Юра купив: " + customerPurchases["Юра"]);
+            Console.WriteLine("Антон купив: " + customerPurchases["Антон"]);
+
+            Console.WriteLine();
+            Console.WriteLine("Клієнти які купили Телефони:");
+            foreach (var (key, value) in customerPurchases)
+            {
+                if (value == "Телефони")
+                {
+                    Console.WriteLine(key);
+                }
+            }
         }
     }
 }
