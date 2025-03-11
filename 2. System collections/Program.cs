3937d42ea2ddcdd45759d7bd93fe5b4521cf51ae
Weretik <vitaliythupin@gmail.com>
2025-03-11 15:31:48 +0200

    Created project: System collections

---
+++ 2. System collections/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2._System_collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
