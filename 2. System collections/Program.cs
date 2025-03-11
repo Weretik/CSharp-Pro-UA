ff81f98f46bc1c6325a1cc17f49ee86e2cf12ecf
Weretik <vitaliythupin@gmail.com>
2025-03-11 15:49:53 +0200

    Completed Task 3. Created collection to store business accounts and available amounts with integer and decimal values.

--- 2. System collections/Program.cs
+++ 2. System collections/Program.cs
@@ -37,6 +37,56 @@ namespace _2._System_collections
                     Console.WriteLine(key);
                 }
             }
+
+
+            /*
+             * Завдання 3
+
+                Декількома способами створіть колекцію, в якій можна зберігати тільки цілі та речові значення, на кшталт «рахунок підприємства – доступна сума» відповідно.
+ 
+             */
+
+            // Перший спосіб
+
+            Dictionary<int, double> accounts = new Dictionary<int, double>();
+
+            accounts.Add(101, 15000.50);
+            accounts.Add(102, 25000.75);
+            accounts.Add(103, 12500.20);
+
+            foreach (var account in accounts)
+            {
+                Console.WriteLine($"Рахунок {account.Key} - доступна сума: {account.Value} грн.");
+            }
+
+
+            // Другий спосіб
+            List<Account> accounts2 = new List<Account>();
+
+            // Додаємо рахунки
+            accounts2.Add(new Account(101, 15000.50));
+            accounts2.Add(new Account(102, 25000.75));
+            accounts2.Add(new Account(103, 12500.20));
+
+            // Виводимо інформацію по рахунках
+            foreach (var account in accounts2)
+            {
+                Console.WriteLine($"Рахунок {account.AccountNumber} - доступна сума: {account.AvailableAmount} грн.");
+            }
+
+
+        }
+
+    }
+    public class Account
+    {
+        public int AccountNumber { get; set; }
+        public double AvailableAmount { get; set; }
+
+        public Account(int accountNumber, double availableAmount)
+        {
+            AccountNumber = accountNumber;
+            AvailableAmount = availableAmount;
         }
     }
 }
