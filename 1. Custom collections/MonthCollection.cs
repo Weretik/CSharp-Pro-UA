288b4d038b364e052f6747bdd337bcbdc89fb0a4
Weretik <vitaliythupin@gmail.com>
2025-03-11 12:29:52 +0200

    Created project: Custom collections. Completed task 2: Implemented collection to store month names, order number, and number of days, with selection functionality by order number and days in month.

---
+++ 1. Custom collections/MonthCollection.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._Custom_collections
+{
+    class MonthCollection: IEnumerable<Month>
+    {
+        Month[] months =
+        {
+            new Month("January", 1, 31),
+            new Month("February", 2, 28),
+            new Month("March", 3, 31),
+            new Month("April", 4, 30),
+            new Month("May", 5, 31),
+            new Month("June", 6, 30),
+            new Month("July", 7, 31),
+            new Month("August", 8, 31),
+            new Month("September", 9, 30),
+            new Month("October", 10, 31),
+            new Month("November", 11, 30),
+            new Month("December", 12, 31)
+        };
+
+        public IEnumerator<Month> GetEnumerator()
+        {
+            foreach (var month in months)
+            {
+                yield return month;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public Month FindByNumber(int number)
+        {
+            return months.FirstOrDefault(m => m.Number == number);
+        }
+
+        public Month[] FindByDays(int days)
+        {
+            return months.Where(m => m.Days == days).ToArray();
+        }
+    }
+
+    public record Month(string Name, int Number, int Days);
+}
--- 1. Custom collections/Program.cs
+++ 1. Custom collections/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace _1._Custom_collections
+﻿using System.Text;
+namespace _1._Custom_collections
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            /*
+             * Завдання 2
+
+            Створіть колекцію, в якій зберігалися б найменування 12 місяців, порядковий номер і кількість днів у відповідному місяці. 
+            Реалізуйте можливість вибору місяців як за порядковим номером, так і кількістю днів у місяці, при цьому результатом може бути не тільки один місяць.
+            */
+
+            MonthCollection months = new MonthCollection();
+
+            foreach (var month in months)
+            {
+                Console.WriteLine(month);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Пошук місяців за номером 5:");
+            Month monthByNumber = months.FindByNumber(5);
+            Console.WriteLine(monthByNumber.Name);
+
+            Console.WriteLine();
+            Console.WriteLine("Пошук місяця за кілкістю днів 30:");
+            Month[] monthByDays = months.FindByDays(30);
+            foreach (var month in monthByDays)
+            {
+                Console.WriteLine(month.Name);
+            }
         }
+        
     }
 }
