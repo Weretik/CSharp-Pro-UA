b4118179644680f2b03f88cab772f6c68e108fca
Weretik <vitaliythupin@gmail.com>
2025-03-13 12:12:59 +0200

    Created Input and Output Programming project. Completed Task 6. Created 100 directories named Folder_0 to Folder_99 and deleted them.

---
+++ 3. Input and Output Programming/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _3._Input_and_Output_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Завдання 6
+
+            Створіть на диску 100 директорій із іменами від Folder_0 до Folder_99, потім видаліть їх.
+            */
+
+            string path = @"C:\Folder";
+            DirectoryInfo directory = new DirectoryInfo(path);
+
+            if (!directory.Exists)
+            {
+                directory.Create();
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                directory.CreateSubdirectory($"Folder_{i}");
+            }
+            //Видалення директорій
+            directory.Delete(true);
+
+        }
+    }
+}
