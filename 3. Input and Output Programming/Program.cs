a2bf56c4712633b69b749a512efc27f0c7fdbf2f
Weretik <vitaliythupin@gmail.com>
2025-03-13 15:05:27 +0200

    Completed Task 2. Created file, wrote random data, closed file, reopened it, read data, and displayed it on the console.

--- 3. Input and Output Programming/Program.cs
+++ 3. Input and Output Programming/Program.cs
@@ -1,14 +1,20 @@
-﻿namespace _3._Input_and_Output_Programming
+﻿using System.Globalization;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace _3._Input_and_Output_Programming
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
             /*Завдання 6
 
             Створіть на диску 100 директорій із іменами від Folder_0 до Folder_99, потім видаліть їх.
             */
-
+            /*
             string path = @"C:\Folder";
             DirectoryInfo directory = new DirectoryInfo(path);
 
@@ -22,7 +28,40 @@
             }
             //Видалення директорій
             directory.Delete(true);
+            */
 
+
+            /*
+             * Завдання 2
+                Створіть файл, запишіть у нього довільні дані та закрийте файл. Потім знову відкрийте цей файл, прочитайте дані і виведіть їх на консоль.
+             * 
+             */
+            string pathFile = @"C:\Users\User\OneDrive\Рабочий стол\C#\Text.txt";
+            FileInfo file = new FileInfo(pathFile);
+
+
+            if (!file.Exists)
+            {
+                using (StreamWriter writer = new StreamWriter(pathFile))
+                {
+                    writer.WriteLine("Мета компанії проста: це забезпечення вам інновацій, відкриттів та новаторства.");
+                    writer.WriteLine("Хочемо вражати стильних особистостей якістю глобального громадянства і прагнемо розвивати кабельне телебачення,");
+                    writer.WriteLine("пасажирські перевезення і ремонт ювелірних виробів і годинників разом із іноземними компаніями.");
+                }
+
+            }
+            else
+            {
+                using (StreamReader reader = new StreamReader(pathFile))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+                file.Delete();
+            }
         }
     }
 }
