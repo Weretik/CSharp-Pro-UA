6489359becf5148e99ee3d8e136b788ccb8817d7
Weretik <vitaliythupin@gmail.com>
2025-03-15 01:47:15 +0200

    Completed Tasks 2, 3, 4, and 6. - Task 2: Created program to display information about a specified .xml file. - Task 3: Extracted and displayed phone numbers from TelephoneBook.xml. - Task 4: Developed admin program to manage configuration data and user interface settings. - Task 6: Created TelephoneBook.xml with "MyContacts" root element and "Contact" tags containing name and "TelephoneNumber" attribute.

--- 5. XML. Configuration files. Registry/Program.cs
+++ 5. XML. Configuration files. Registry/Program.cs
@@ -1,2 +1,101 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Xml.Linq;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json; 
+
+/*Завдання 6 
+
+Створіть .xml файл, який би відповідав наступним вимогам:
+
+• ім'я файлу: TelephoneBook.xml
+
+• кореневий елемент: “MyContacts”
+
+• тег “Contact”, і в ньому має бути записано ім'я контакту та атрибут “TelephoneNumber” зі значенням номера телефону
+*/
+
+
+
+XDocument telephoneBook1 = new XDocument(new XElement("MyContacts",
+    new XElement("Contact",
+        new XAttribute("TelephoneNumber", "+(380) 96 087 77 08"),
+        "Ivan"),
+    new XElement("Contact",
+        new XAttribute("TelephoneNumber", "+(380) 96 087 77 09"),
+        "Victor")));
+
+telephoneBook1.Save(@"C:\Users\User\OneDrive\Рабочий стол\C#\C# Pro UA\5. XML. Configuration files. Registry\TelephoneBook.xml");
+
+
+
+/*
+ * Завдання 2
+
+Створіть програму, яка виводить на екран всю інформацію про вказаний .xml файл.
+*/
+
+
+XDocument telephoneBook = XDocument.Load(@"C:\Users\User\OneDrive\Рабочий стол\C#\C# Pro UA\5. XML. Configuration files. Registry\TelephoneBook.xml");
+
+XElement root = telephoneBook.Element("MyContacts");
+
+Console.WriteLine(root);
+
+
+/*
+Завдання 3
+
+З файлу TelephoneBook.xml (файл повинен був бути створений у процесі виконання додаткового завдання) виведіть на екран лише номери телефонів.
+*/
+Console.WriteLine();
+if (root != null)
+{
+    foreach (XElement contact in root.Elements("Contact"))
+    {
+        XAttribute telephoneNumber = contact.Attribute("TelephoneNumber");
+        if (telephoneNumber != null)
+        {
+            Console.WriteLine($"Telephone number: {telephoneNumber.Value}");
+            Console.WriteLine();
+        }
+    }
+}
+
+/*Завдання 4 
+
+Використовуючи приклади, розглянуті на уроці, створіть свою програму для адміністратора, 
+яка зберігатиме дані конфігурації у спеціальному файлі або в реєстрі. Створіть програму користувача, 
+зовнішнім виглядом якого можна керувати за допомогою адмінпрограми.
+ * 
+ */
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile(@"C:\Users\User\OneDrive\Рабочий стол\C#\C# Pro UA\5. XML. Configuration files. Registry\settings.json")
+    .Build();
+
+var consoleColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), configuration["Application:ConsoleColor"]);
+Console.ForegroundColor = consoleColor;
+
+
+var envConfig = configuration.GetSection("Environment").Get<EnvironmentConfig>();
+
+Console.WriteLine($"Database Name: {envConfig.Database.Name}");
+Console.WriteLine($"Connection String: {envConfig.Database.ConnectionString}");
+
+Console.WriteLine(new string('-', 30));
+
+Console.WriteLine("Hello world!");
+
+Console.ReadLine();
+
+
+public class EnvironmentConfig
+{
+    public DatabaseConfig Database { get; set; }
+}
+
+public class DatabaseConfig
+{
+    public string Name { get; set; }
+    public string ConnectionString { get; set; }
+}
