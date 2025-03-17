77362d9b38a7c73154528e15dd7317630efbd8a7
Weretik <vitaliythupin@gmail.com>
2025-03-18 00:11:12 +0200

    Completed Task 2. Created class with serialization support and serialized object to XML. First using default format, then modified to store field values as XML attributes.

--- 8. Serialization/Program.cs
+++ 8. Serialization/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace _8._Serialization
+﻿using System.Xml.Serialization;
+
+namespace _8._Serialization
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+             * Завдання 2
+
+            Створіть клас, який підтримує серіалізацію. Виконайте серіалізацію цього об'єкта у форматі XML.
+            Спочатку використовуйте формат за промовчанням, а потім змініть його таким чином,
+            щоб значення полів збереглися як атрибути елементів XML.
+            */
+            Person person = new Person("Віктор", 25, new Company("Microsoft"));
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Person));
+
+            using(FileStream fs = new FileStream("person.xml", FileMode.OpenOrCreate))
+            {
+                serializer.Serialize(fs, person);
+            }
+
+
+
+        }
+    }
+    public class Person(string name, int age, Company company)
+    {
+        public string Name { get; set; } = name;
+        public int Age { get; set; } = age;
+
+        public Company Company { get; set; } = company;
+
+        public Person() : this("Underfined", 0, new Company("Underfined"))
+        {
+       
+        }
+
+
+    }
+    public class Company(string name)
+    {
+        [XmlAttribute]
+        public string Name { get; set; } = name;
+
+        public Company() : this("Underfined")
+        {
         }
     }
 }
