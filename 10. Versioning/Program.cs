664affa00b91af33c5cb66d8800ff2613caf590c
Weretik <vitaliythupin@gmail.com>
2025-03-18 17:54:37 +0200

    feat(template-method): Implement abstract template method (Task 2).

--- 10. Versioning/Program.cs
+++ 10. Versioning/Program.cs
@@ -4,7 +4,69 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+             * Завдання 2
+
+            Вивчіть опис шаблону Template method (Шаблонний метод). Зверніть увагу на застосування шаблону, а також на склад його учасників і зв'язку відносини між ними. 
+            Напишіть невелику програму мовою C#, що є абстрактною реалізацією даного шаблону.
+            */
+
+            Console.WriteLine("Обробка текстового документа:");
+            DocumentProcessor textProcessor = new TextDocumentProcessor();
+            textProcessor.ProcessDocument();
+
+            Console.WriteLine("\nОбробка XML-документа:");
+            DocumentProcessor xmlProcessor = new XmlDocumentProcessor();
+            xmlProcessor.ProcessDocument();
+        }
+    }
+    abstract class DocumentProcessor
+    {
+        public void ProcessDocument()
+        {
+            OpenDocument();
+            ParseDocument();
+            SaveDocument();
+            CloseDocument();
+        }
+
+        protected void OpenDocument()
+        {
+            Console.WriteLine("Документ відкрито.");
+        }
+
+        protected void CloseDocument()
+        {
+            Console.WriteLine("Документ закрито.");
+        }
+
+        protected abstract void ParseDocument();
+        protected abstract void SaveDocument();
+    }
+
+    class TextDocumentProcessor : DocumentProcessor
+    {
+        protected override void ParseDocument()
+        {
+            Console.WriteLine("Аналіз текстового документа...");
+        }
+
+        protected override void SaveDocument()
+        {
+            Console.WriteLine("Збереження документа у текстовому форматі.");
+        }
+    }
+
+    class XmlDocumentProcessor : DocumentProcessor
+    {
+        protected override void ParseDocument()
+        {
+            Console.WriteLine("Аналіз XML-документа...");
+        }
+
+        protected override void SaveDocument()
+        {
+            Console.WriteLine("Збереження документа у форматі XML.");
         }
     }
 }
