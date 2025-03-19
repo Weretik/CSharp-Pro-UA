dfdade3e5bf7d86e8d20067f324f19de7885f172
Weretik <vitaliythupin@gmail.com>
2025-03-19 18:30:41 +0200

    feat(mutex): Ensure single-instance execution using named mutex (task 3).

--- 12. Synchronization/Program.cs
+++ 12. Synchronization/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+             * Завдання 3
+
+            Створіть програму, яка може бути запущена лише в одному екземплярі (використовуючи іменований Mutex).
+            */
+            bool isMutexAcquired = false;
+            Mutex mutex = new Mutex(true, "UniqueMutex", out isMutexAcquired);
+
+            if (!isMutexAcquired)
+            {
+                Console.WriteLine("Програма вже запущена!");
+                return;
+            }
+
+            Console.WriteLine("Програма успішно запущена!");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Цикл: {i + 1}: *");
+                Thread.Sleep(500);
+            }
+
+            Console.WriteLine("Програма завершена. Натисніть будь-яку клавішу для виходу...");
+
+            mutex.ReleaseMutex();
         }
     }
 }
