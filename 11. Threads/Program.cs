1810640d9e030696ea4343d3a66eaba2204d2206
Weretik <vitaliythupin@gmail.com>
2025-03-19 11:36:59 +0200

    feat(threads): Initialize project 11 (Threads).

---
+++ 11. Threads/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _11._Threads_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
