c625b58f03e034766796bc9fc22d0013db02e4ea
Weretik <vitaliythupin@gmail.com>
2025-03-21 17:56:08 +0200

    feat(plinq): Generate large array and use PLINQ to filter odd numbers (task 4).

--- 13. TPL/Program.cs
+++ 13. TPL/Program.cs
@@ -1,13 +1,18 @@
-﻿namespace _13._TPL
+﻿using System.Text;
+
+namespace _13._TPL
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
             /*
              * Завдання 2
 
-            Створіть два методи, які виконуватимуться у межах паралельних завдань. Організуйте виклик цих методів за допомогою Invoke таким чином, щоб основний потік програми (метод Main) не зупинив виконання.
+            Створіть два методи, які виконуватимуться у межах паралельних завдань. Організуйте виклик цих методів за допомогою Invoke таким чином, 
+            щоб основний потік програми (метод Main) не зупинив виконання.
             */
             Parallel.Invoke(
                 () => PrintGreetings(),
@@ -21,7 +26,31 @@
                 Console.WriteLine(" * ");
             }
             Console.WriteLine("Основнйи потік закінчив роботу");
+            Console.WriteLine();
+
+            /*
+             * Завдання 4
+
+            Створіть масив чисел розмірністю 1000000 або більше. Використовуючи генератор випадкових чисел, проініціалізуйте цей масив значеннями. 
+            Створіть PLINQ запит, який дозволить отримати усі непарні числа з вихідного масиву.
+            */
+            Random random = new Random();
+
+            int[] numbers = new int[1000000];
+            
+            Parallel.For(0, numbers.Length, (i) =>
+            {
+                numbers[i] = random.Next(0, 100);
+            });
+
+            var results = numbers
+                .AsParallel()
+                .Where(n => n % 2 != 0)
+                .ToArray();
+
+
         }
+        //* Завдання 2
         static void PrintGreetings()
         {
             Console.WriteLine($"Поточний потік: {Thread.CurrentThread.ManagedThreadId}");
