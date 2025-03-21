472b8b8c8658cc491d3948501e6bd8f919eececf
Weretik <vitaliythupin@gmail.com>
2025-03-21 17:42:14 +0200

    feat(parallel-tasks): Implement non-blocking parallel method execution (task 2).

--- 13. TPL/Program.cs
+++ 13. TPL/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
+            /*
+             * Завдання 2
+
+            Створіть два методи, які виконуватимуться у межах паралельних завдань. Організуйте виклик цих методів за допомогою Invoke таким чином, щоб основний потік програми (метод Main) не зупинив виконання.
+            */
+            Parallel.Invoke(
+                () => PrintGreetings(),
+                () => Square(5)
+            );
+
+            Console.WriteLine($"Поточний потік: {Thread.CurrentThread.ManagedThreadId}");
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine(" * ");
+            }
+            Console.WriteLine("Основнйи потік закінчив роботу");
+        }
+        static void PrintGreetings()
+        {
+            Console.WriteLine($"Поточний потік: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(200);
             Console.WriteLine("Hello, World!");
+        }
+        static void Square(int x)
+        {
+            Console.WriteLine($"Поточний потік: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(200);
+            Console.WriteLine(x * x);
         }
 
     }
