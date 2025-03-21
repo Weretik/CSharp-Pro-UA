69cab9ede321a7da76a946e241647e6f7e9e3e44
Weretik <vitaliythupin@gmail.com>
2025-03-21 17:39:18 +0200

    feat(threads): Initialize project 13 (TPL).

---
+++ 13. TPL/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _13._TPL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+}
